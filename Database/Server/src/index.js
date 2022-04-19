f6fdf24cb22649230838f1686285c5e73ae96a5a
dpnandkeshwar <dpnandkeshwar@gmail.com>
2022-04-19 02:03:22 -0600

    simplify test code

--- Database/Server/src/index.js
+++ Database/Server/src/index.js
@@ -1,24 +1,8 @@
-import 'dotenv/config';
 import express from 'express';
-import cors from 'cors';
 const app = express();
 
-app.use(cors());
-
-app.get('/users', (req, res) => {
+app.get('/', (req, res) => {
     return res.send('Received a GET HTTP method');
-});
-
-app.post('/', (req, res) => {
-    return res.send('Received a POST HTTP method');
-});
-
-app.put('/', (req, res) => {
-    return res.send('Received a PUT HTTP method');
-});
-
-app.delete('/', (req, res) => {
-    return res.send('Received a DELETE HTTP method');
 });
 
 app.listen(process.env.PORT || '3000', () =>
