97fa180fcb1a8550a71f9749f693819e1c0563ae
Justin Rogosienski <u1134889@umail.utah.edu>
2022-04-23 16:52:39 -0600

    Progress on reaching the database

--- KDC/kdc-worker/src/dataConnector.ts
+++ KDC/kdc-worker/src/dataConnector.ts
@@ -25,20 +25,29 @@ type UserData = {
 }
 
 export async function getLongTermSecret(user: string): Promise<KeyIv> {
-    // const body = {ID: user};
-    
-
-    // const response = await fetch('https://distributedkdc.azurewebsites.net/api/users', {
-    //     method: 'post',
-    //     body: JSON.stringify(body),
-    //     headers: {'Content-Type': 'application/json'}
-    // });
-    // const data: any= await response.json();
+    const body = {ID: user};
+    // const ENC_KEY = Buffer.from('f1ed01c4f55def71bcc224b061d901ce2797b1d2a9a2d9f29b1b2cfd4d4fd2ac', 'hex');
+    // const IV = Buffer.from("9ba6c0af38bc092afdd9cf746b138de9", 'hex');
+    try {
+        const response = await fetch('https://distributedkdc.azurewebsites.net/apiold/users/getuser', {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {'Content-Type': 'application/json'}
+        });
+        console.log("It worked!!")
+        console.log(await response.text());
+    }
+    catch (e) {
+        console.log("Error happened")
+        console.log(e);
+    }
     // const obj = data.recordset[0];
     // return {
     //     key: obj.KeyBytes,
     //     iv: obj.IV
     // }
+
+
     return new Promise((resolve, reject)=>{
         resolve({
             key: Buffer.from([180, 143, 232, 239, 1, 187, 8, 122, 15, 70, 75, 87, 247, 218, 187, 79, 247, 151, 136, 166, 103, 141, 134, 124, 78, 121, 224, 247, 86, 140, 54, 112]),
--- KDC/kdc-worker/src/handler.ts
+++ KDC/kdc-worker/src/handler.ts
@@ -4,7 +4,7 @@ export async function handleRequest(request: Request): Promise<Response> {
   const body: any = await request.json<SessionRequest>()
   console.log(JSON.stringify(body));
   const kdcResponse = await getKdcResponse(body.n1, body.alice, body.bob, body.nB);
-  return new Response(`request method: ${request.method}`)
+  return new Response(JSON.stringify(kdcResponse));
 }
 
 type SessionRequest = {
