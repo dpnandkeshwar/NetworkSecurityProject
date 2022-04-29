e1bf3ce9a9ef506694a0ecfc12b5765a151eb59f
Justin Rogosienski <u1134889@umail.utah.edu>
2022-04-29 01:59:45 -0600

    Updates output of Alice

--- Alice/Alice.ts
+++ Alice/Alice.ts
@@ -2,6 +2,7 @@ import { assert } from 'console';
 import * as crypto from 'crypto';
 import fetch from 'node-fetch';
 import * as net from 'net';
+import { logBreak, logTitledJson, logTitledObject, logTitledString } from './outputUtil';
 
 
 // type AliceToKDC = { n1: string, alice: string, bob : string,  nB : string};
@@ -18,7 +19,7 @@ async function request(toKDC : string, url : string) : Promise<string> {
         body: toKDC
     });
     const body = await response.json();
-    console.log("---------Encrypted  json received from the KDC ------------")
+    // console.log("---------Encrypted  json received from the KDC ------------")
     //console.log(body.data);
     return body.data;
 }
@@ -31,12 +32,22 @@ function getDecryptedJson(encrypted : string){
 
     try{
         let decipher = crypto.createDecipheriv(algorithm, keyBytes,ivBytes);
-        let decrypted = decipher.update(encrypted, 'base64', 'base64');
-        decrypted += decipher.final('base64');
-        console.log("---------Decrypting the data returns the following output-----------")
-        let kdcResponse = Buffer.from(decrypted, 'base64').toString('utf8');
-        console.log(kdcResponse);
-        return kdcResponse;
+        let decrypted = decipher.update(encrypted, 'base64', 'utf-8');
+        decrypted += decipher.final('utf-8');
+        // console.log("---------Decrypting the data returns the following output-----------")
+        // console.log(kdcResponse);
+        const kdcResponse = JSON.parse(decrypted);
+        const cleanResponse = {
+            nonce: kdcResponse.nonce,
+            bob: kdcResponse.bob,
+            sessionKey: {
+                key: Buffer.from(kdcResponse.sessionKey.key.data).toString('hex'),
+                iv: Buffer.from(kdcResponse.sessionKey.iv.data).toString('hex')
+            },
+            ticket: kdcResponse.ticket
+        }
+        logTitledObject('DECRYPTED RESPONSE FROM KDC', cleanResponse);
+        return decrypted;
     }
     catch(e){
         console.error("Error happened when decrypting")
@@ -54,13 +65,15 @@ function sendLastMessageToBob(n3 : string, keyBytes : Buffer, ivBytes : Buffer){
     const newBuff = Buffer.allocUnsafe(8);
     newBuff.writeBigInt64BE(n3Int, 0);
     const n3_1 = newBuff.toString('base64');
-    console.log(`Computed n3-1: ${n3_1}` );
+    // console.log(`Computed n3-1: ${n3_1}` );
+    logTitledString('COMPUTED n3 - 1', n3_1);
 
     //encrypt n3_1 
     let cipher = crypto.createCipheriv(algorithm, keyBytes, ivBytes);
     let encryptedN3_1 = cipher.update(n3, 'base64', 'base64');
     encryptedN3_1 += cipher.final('base64');
-    console.log(`Encrypted n3-1: ${encryptedN3_1}` );
+    // console.log(`Encrypted n3-1: ${encryptedN3_1}` );
+    logTitledString('ENCRYPTED n3 - 1', encryptedN3_1)
 
 
     //send it to Bob
@@ -81,24 +94,30 @@ function aliceAndBob(kdcResponse : any){
     let encryptedReply = aliceCipher.update(n2, 'base64', 'base64');
     encryptedReply += aliceCipher.final('base64');
 
-    console.log("----Alice and Bob communication-------");
-    console.log("N2 nonce created: " + n2);
-    console.log("Encrypted N2: " + encryptedReply);
+    // console.log("----Alice and Bob communication-------");
+    logBreak('ALICE AND BOB COMMUNICATION');
+    // console.log("N2 nonce created: " + n2);
+    logTitledString('GENERATED NONCE n2', n2);
+    // console.log("Encrypted N2: " + encryptedReply);
+    logTitledString('ENCRYPTED NONCE n2', encryptedReply);
     
     const toBob = {"n2" : encryptedReply, "ticket" : kdcResponse.ticket};
-    console.log("Sending to Bob: " + JSON.stringify(toBob));
+    // console.log("Sending to Bob: " + JSON.stringify(toBob));
+    logTitledObject('SENDING TO BOB', toBob);
 
     var client = net.connect({host: '127.0.0.1', port: 3000}, function() {
         client.write(JSON.stringify(toBob));
     });
 
     client.on('data',function(data){
-        console.log("Received from Bob: " + data.toString());
+        // console.log("Received from Bob: " + data.toString());
+        logTitledJson('RECEIVED FROM BOB', data.toString());
         const fromBob = JSON.parse(data.toString());
         let decipher = crypto.createDecipheriv(algorithm, keyBytes, ivBytes);
         let decryptedN2_1 = decipher.update(fromBob.n2_1, 'base64', 'base64');
         decryptedN2_1 += decipher.final('base64'); 
-        console.log("Decrypted n2 - 1 received from Bob: " + decryptedN2_1);
+        // console.log("Decrypted n2 - 1 received from Bob: " + decryptedN2_1);
+        logTitledString('EXPECTING n2 - 1 (decrypted response above)', decryptedN2_1);
 
         //compute the expected n2 - 1
         const buff = Buffer.from(n2, 'base64');
@@ -106,12 +125,14 @@ function aliceAndBob(kdcResponse : any){
         const newBuff = Buffer.allocUnsafe(8);
         newBuff.writeBigInt64BE(n2Int, 0);
         const n2_1 = newBuff.toString('base64');
-        console.log("Expected n2 - 1: " + n2_1 );
+        // console.log("Expected n2 - 1: " + n2_1 );
+        logTitledString('EXPECTED n2 - 1', n2_1);
 
         decipher = crypto.createDecipheriv(algorithm, keyBytes, ivBytes);
         let decryptedN3 = decipher.update(fromBob.n3, 'base64', 'base64');
         decryptedN3 += decipher.final('base64'); 
-        console.log(`Decrypted n3: ${decryptedN3}`);
+        // console.log(`Decrypted n3: ${decryptedN3}`);
+        logTitledString('DECRYPTED n3', decryptedN3);
 
         sendLastMessageToBob(decryptedN3, keyBytes, ivBytes);
     });
@@ -126,7 +147,8 @@ let n1 = crypto.randomBytes(8).toString('base64');
 let nB = '';
 
 var client = net.connect({host: '127.0.0.1', port: 3000}, function() {
-    console.log('Asking bob for nonce');  
+    // console.log('Asking bob for nonce');
+    logTitledString('REQUESTING BOB\'S NONCE', 'I want to talk');
     client.write("I want to talk");
 });
 
@@ -134,20 +156,24 @@ client.on('data',function(data){
     nB = JSON.parse(data.toString()).nB;
     let toKDC = JSON.stringify({n1, alice : aliceId, bob : bobId, nB});
 
-    console.log(toKDC);
+    // console.log(toKDC);
+    logTitledJson('TO KDC', toKDC);
 
     (async () => {
         // const encrypted = await request(toKDC, "http://localhost:8787");
 
     
         const encrypted = await request(toKDC, "http://127.0.0.1:8787");
-        console.log(encrypted);
+        // console.log(encrypted);
+        logTitledString('ENCRYPTED RESPONSE FROM KDC', encrypted);
 
         const kdcResponse = JSON.parse(getDecryptedJson(encrypted));
 
-        console.log("----Verify the nonce-------");
-        console.log("Nonce received from KDC: " + kdcResponse.nonce);
-        console.log("Correct nonce: " + n1);
+        // console.log("----Verify the nonce-------");
+        // console.log("Nonce received from KDC: " + kdcResponse.nonce);
+        logTitledString('EXPECTING MY NONCE n1 BACK FROM KDC', kdcResponse.nonce);
+        // console.log("Correct nonce: " + n1);
+        logTitledString('CORRECT NONCE n1                   ', n1);
         assert(n1 == kdcResponse.nonce);
 
         aliceAndBob(kdcResponse);
---
+++ Alice/outputUtil.ts
@@ -0,0 +1,15 @@
+export function logTitledJson(name: string, dataJson: string) {
+    console.log(`\x1b[34m${name}\x1b[0m\n${JSON.stringify(JSON.parse(dataJson), undefined, 2)}`);
+}
+
+export function logTitledObject(name: string, data: Object) {
+    console.log(`\x1b[34m${name}\x1b[0m\n${JSON.stringify(data, undefined, 2)}`);
+}
+
+export function logTitledString(name: string, data: string) {
+    console.log(`\x1b[34m${name}\x1b[0m\n${data}`);
+}
+
+export function logBreak(message: string) {
+    console.log(`\n\x1b[31m--------- ${message} ---------\x1b[0m\n`);
+}
